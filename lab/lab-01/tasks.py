31b0118f43658c0634441f30240e13eecd582bbc
Tri Phan <phanphutri@gmail.com>
2023-09-20 17:25:51 +0700

    Updated

--- lab/lab-01/tasks.py
+++ lab/lab-01/tasks.py
@@ -1,3 +1,8 @@
+'''
+Lab 1: Chương trình đầu tiên và chú thích (bài 1 đến 6)
+Link: https://codelearn.io/learning/python-co-ban
+'''
+
 ##
 # Task 1
 # Fill the blank (...) with appropriate statements to print the following line on the screen:
@@ -7,3 +12,66 @@
 # print('Hello, World!')
 
 # --------------------------------------------------------------------
+
+##
+# Task 2
+# Fill the blank (...) with appropiate statements to print the following line on the screen:
+# Welcome to Codelearn.io!
+#
+
+# print('Welcome to Codelearn.io!')
+
+# --------------------------------------------------------------------
+
+##
+# Task 3
+# Write a program to print the sum of 3273 and 2282 on the screen.
+#
+
+# print(3273 + 2282)
+
+# --------------------------------------------------------------------
+
+##
+# Task 4
+# Write a program to find the sum, difference, production, and quotient of 2468 and 1234
+# and print the following lines on the screen:
+#
+
+# x = 2468
+# y = 1234
+# print(f'{x} + {y} = {x+y}')
+# print(f'{x} - {y} = {x-y}')
+# print(f'{x} * {y} = {x*y}')
+# print(f'{x} / {y} = {x/y}')
+
+# --------------------------------------------------------------------
+
+##
+# Task 5
+# Given a rectangle with a width of 6.4 and a length of 7.8.
+# Write a program to print the following lines on the screen:
+# Area = {P1}
+# Perimeter = {P2}
+#
+
+# width = 6.4
+# length = 7.8
+
+# print(f'Area = {width*length}')
+# print(f'Perimeter = {(width+length)*2}')
+
+# --------------------------------------------------------------------
+
+##
+# Task 6
+# Use Python's comments to complete a program which prints the following text on the screen:
+# Hello, World!
+#
+
+'''
+A simple Python program to display "Hello, World!" on the screen
+using Python's print() function
+'''
+# Display Hello World on the screen
+print("Hello, World!")
---
+++ lab/lab-02/lab-2-1/tasks.py
@@ -0,0 +1,115 @@
+'''
+Lab 2.1: Kiểu dữ liệu, biến và ép kiểu (từ bài 7 đến 12)
+Link: https://codelearn.io/learning/python-co-ban
+'''
+
+##
+# Task 7
+# Run the sample code in the right pane to know how variables are initialized
+# and how their values are printed on the screen.
+#
+
+# # Initialization an integer variable name num_integer
+# num_integer = 1000
+# # Initialization a float variable name num_float
+# num_float = 3.125
+# # Initialization a string variable name string_var
+# string_var = 'Codelearn.io'
+# # Initialization a boolean variable name boolean_var
+# boolean_var = True
+
+# # Print value of each variable
+# print(num_integer)
+# print(num_float)
+# print(string_var)
+# print(boolean_var)
+
+# --------------------------------------------------------------------
+
+##
+# Task 8
+# Fill the blank (...) to complete a program that
+# prints the following information on the screen:
+# 5000
+# 1.2345
+# Codelearn.io
+# False​
+#
+
+# num_integer = 5000
+# num_float = 1.2345
+# string_var = 'Codelearn.io'
+# boolean_var = False
+
+# print(num_integer)
+# print(num_float)
+# print(string_var)
+# print(boolean_var)
+
+# --------------------------------------------------------------------
+
+##
+# Task 9
+# Write a program that creates a variable and its value is "Codelearn"
+# then prints the following text on the screen:
+# Hello Codelearn
+#
+
+# name = "Codelearn"
+# print('Hello ' + name)
+
+# --------------------------------------------------------------------
+
+##
+# Task 10
+# Write a program that creates variables: name = "Codelearn",  date_of_birth = 2019
+# and prints the following information on the screen:
+# Name: Codelearn
+# Date of birth: 2019
+#
+
+# name = 'Codelearn'
+# date_of_birth = 2019
+# print("Name: " + name)
+# print("Date of birth: " + str(date_of_birth))
+
+# --------------------------------------------------------------------
+
+##
+# Task 11
+# Write a program to create variables: a = 438, b = 636
+# and print sum, difference, product, and quotient of a and b as below:
+# a + b = {P1}
+# a - b = {P2}
+# a * b = {P3}
+# a / b = {P4}
+#
+
+# a = 438
+# b = 636
+# add = a + b
+# sub = a - b
+# mul = a * b
+# div = a / b
+# print("a + b = " + str(add))
+# print("a - b = " + str(sub))
+# print("a * b = " + str(mul))
+# print("a / b = " + str(div))
+
+# --------------------------------------------------------------------
+
+##
+# Task 12
+# Given variables length = 7.8 and width = 3.5 which are the length and width of a rectangle respectively.
+# Write a program to print the area and perimeter of the given rectangle as below:
+# Area: {P1}
+# Perimeter: {P2}
+#
+
+# length = 7.8
+# width = 3.5
+# area = length * width
+# perimeter = (length + width) * 2
+
+# print(f'Area: {area}')
+# print(f'Perimeter: {perimeter}')
