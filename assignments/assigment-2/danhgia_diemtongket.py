d7c5c11b83bd655412a263fcdbe381ed016ea3d2
Tri Phan <phanphutri@gmail.com>
2023-09-25 19:52:08 +0700

    Updated

---
+++ assignments/assigment-2/danhgia_diemtongket.py
@@ -0,0 +1,14 @@
+
+from tinhtoan_diemtongket import get_full_path
+
+
+def xeploai_hocsinh(file_name):
+    
+
+
+def main():
+    file_name = 'diem_trungbinh.txt'
+
+
+if __name__ == '__main__':
+    main()
